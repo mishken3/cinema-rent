00030252ea7b457464cf5b47ed47527fab853aea
Mikhail <mishken3@gmail.com>
2022-07-28 15:20:47 +0300

    install plugins && linter fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import './App.css';
 
+import React from 'react';
+
 function App() {
-	return (
-		<div>
-			<p>App.tsx</p>
-			<p>Cinema rent website</p>
-		</div>
-	);
+  return (
+    <div>
+      <p>App.tsx</p>
+      <p>Cinema rent website</p>
+    </div>
+  );
 }
 
 export default App;

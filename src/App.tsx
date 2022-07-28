fcd05613a58d6cecc253764b97fd7c7ddeda4800
Mikhail <mishken3@gmail.com>
2022-07-28 17:49:01 +0300

    add fonts && small linter fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import 'normalize.css';
 import './App.css';
 
 import React from 'react';
 
+import { AdressBar } from './Components';
+
 function App() {
   return (
     <div>
+      <AdressBar />
       <p>App.tsx</p>
       <p>Cinema rent website</p>
     </div>

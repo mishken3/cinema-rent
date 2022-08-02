a96f4ae83aa3fde586c32e6235559ec8cb41bd50
Mikhail <mishken3@gmail.com>
2022-08-02 17:37:25 +0300

    add nav border svg

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,6 @@
 export { default as cameraBG } from './camera-bg.png';
 export { default as mainLogo } from './main-logo.svg';
 export * as navImg from './nav';
+export { default as navBorder } from './nav-border.svg';
 export * as partnersImg from './partners';
 export { default as transferBG } from './transfer-bg.png';
